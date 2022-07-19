09a879862061621aa6ccb6628c6270a3d59cb6a7
gkurkov <gkurkov@gmail.com>
2022-07-19 13:43:02 +0300

    task done

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,37 +1,36 @@
-import React from "react";
+import React from 'react'
 import Quality from './quality'
 import Bookmark from './bookmark'
 
-const User = ( {user, props} ) => {
-
-     const qualities = user.qualities
-     return  <tbody>
-                    <tr key={user._id}>
-                        <td>{user.name}</td>
-                        <td>
-
-                        {qualities.map((item) =>  {
-                            return <Quality quality = {item} />
-                        })}
-     
-                        </td>
-                        <td>{user.profession.name}</td>
-                        <td>{user.completedMeetings}</td>
-                        <td>{user.rate}/5</td>
-                        <td>
-                        <Bookmark state = {user.bookmark} id = {user._id} props = {props}/>
-
-                        </td>
-                        <td>
-                            <button
-                                className="btn btn-danger" id={user._id}
-                                onClick = {() => props.onDelete(user._id)}
-                            >
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-        </tbody>
+const User = ({ user, props }) => {
+  const qualities = user.qualities
+  return (
+    <tbody>
+      <tr>
+        <td>{user.name}</td>
+        <td>
+          {qualities.map((item) => {
+            return <Quality quality={item} key={item._id} />
+          })}
+        </td>
+        <td>{user.profession.name}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate}/5</td>
+        <td>
+          <Bookmark state={user.bookmark} id={user._id} props={props} />
+        </td>
+        <td>
+          <button
+            className='btn btn-danger'
+            id={user._id}
+            onClick={() => props.onDelete(user._id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  )
 }
 
-export default User 
+export default User
