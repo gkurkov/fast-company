6d01177b7a41d57eee3efa049af6574ad5f980e0
gkurkov <gkurkov@gmail.com>
2022-08-03 23:27:26 +0300

    up or down arrow

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     plugins: ['react'],
     rules: {
-        indent: ['error', 4],
+        indent: [0, 4],
         semi: ['error', 'never'],
         'space-before-function-paren': [
             'error',
